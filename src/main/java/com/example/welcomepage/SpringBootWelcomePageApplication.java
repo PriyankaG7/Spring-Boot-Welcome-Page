eb71622521ab30d56588e501fa3ae97bd5f39852
Priyanka <priyankagurrana@gmail.com>
2025-08-01 18:13:58 -0500

    Initial commit of Spring Boot project

---
+++ src/main/java/com/example/welcomepage/SpringBootWelcomePageApplication.java
@@ -0,0 +1,13 @@
+package com.example.welcomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWelcomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWelcomePageApplication.class, args);
+	}
+
+}
